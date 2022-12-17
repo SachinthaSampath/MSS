fd754150f2e41030c4ae134371313868eff1eb25
Chathu0508 <chathuranga.karunarathne0508@gmail.com>
2022-12-17 14:38:55 +0530

    commit 003

--- page.php
+++ page.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <h1>my page</h1>
+    <?php
+    //to print multiple times use the same one over and over. 
+    printname();
+    doCal();
+    echo "Return Value = $myreturn <br>";
+
+    function printname()
+    {
+        echo "my name is Chathuranga.";
+        echo "<br>my name is Shithari.<br>";
+    }
+
+    function doCal()
+    {
+        $val1 = 50;
+        $val2 = 60;
+
+        $total = $val1 + $val2;
+        echo "Total:$total<br>";
+    }
+    ?>
+
 </body>
+
 </html>
